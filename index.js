b0da6e51b7b86591201a4d0deb1e93f4d255fc9a
Thomas Rempel <80909030+ThomasRempel@users.noreply.github.com>
2022-07-01 17:44:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ function fertine() {
     var RN = document.getElementById("RN");
     var CE = document.getElementById("CE");
     var PI = document.getElementById("PI");
+    var boxFertine = document.getElementById("boxFertine");
     PE.classList.add("fill_svg");
     PB.classList.add("fill_svg");
     RN.classList.add("fill_svg");
     CE.classList.add("fill_svg");
     PI.classList.add("fill_svg");
+    boxFertine.style.display = "block";
+}
+
+function fertineBox() {
+    var PE = document.getElementById("PE");
+    var PB = document.getElementById("PB");
+    var RN = document.getElementById("RN");
+    var CE = document.getElementById("CE");
+    var PI = document.getElementById("PI");
+    var boxFertine = document.getElementById("boxFertine");
+    PE.classList.remove("fill_svg");
+    PB.classList.remove("fill_svg");
+    RN.classList.remove("fill_svg");
+    CE.classList.remove("fill_svg");
+    PI.classList.remove("fill_svg");
+    boxFertine.style.display = "none";
 }
 
 function maranhao(){
@@ -16,33 +33,82 @@ function maranhao(){
     var PI = document.getElementById("PI");
     var PA = document.getElementById("PA");
     var TO = document.getElementById("TO");
+    var boxMaranhao = document.getElementById("boxMaranhao");
     MA.classList.add("fill_svg");
     PI.classList.add("fill_svg");
     PA.classList.add("fill_svg");
     TO.classList.add("fill_svg");
+    boxMaranhao.style.display = "block";
+}
+
+function maranhaoBox(){
+    var MA = document.getElementById("MA");
+    var PI = document.getElementById("PI");
+    var PA = document.getElementById("PA");
+    var TO = document.getElementById("TO");
+    var boxMaranhao = document.getElementById("boxMaranhao");
+    MA.classList.remove("fill_svg");
+    PI.classList.remove("fill_svg");
+    PA.classList.remove("fill_svg");
+    TO.classList.remove("fill_svg");
+    boxMaranhao.style.display = "none";
 }
 
 function fertial(){
     var AL = document.getElementById("AL");
     var CE = document.getElementById("CE");
     var RN = document.getElementById("RN");
+    var boxFertial = document.getElementById("boxFertial");
     AL.classList.add("fill_svg");
     CE.classList.add("fill_svg");
     RN.classList.add("fill_svg");
+    boxFertial.style.display = "block";
+}
+function fertialBox(){
+    var AL = document.getElementById("AL");
+    var CE = document.getElementById("CE");
+    var RN = document.getElementById("RN");
+    var boxFertial = document.getElementById("boxFertial");
+    AL.classList.remove("fill_svg");
+    CE.classList.remove("fill_svg");
+    RN.classList.remove("fill_svg");
+    boxFertial.style.display = "none";
 }   
 
 function matogrosso(){
     var MT = document.getElementById("MT");
-    MT.classList.add("fill_svg");  
+    var boxMatogrosso = document.getElementById("boxMatogrosso");
+    MT.classList.add("fill_svg"); 
+    boxMatogrosso.style.display = "block"; 
+}
+
+function matogrossoBox(){
+    var MT = document.getElementById("MT");
+    var boxMatogrosso = document.getElementById("boxMatogrosso");
+    MT.classList.remove("fill_svg"); 
+    boxMatogrosso.style.display = "none"; 
 }
 
 function fertigran(){
     var MG = document.getElementById("MG");
     var GO = document.getElementById("GO");
     var SP = document.getElementById("SP");
+    var boxFertigran = document.getElementById("boxFertigran");
     MG.classList.add("fill_svg"); 
     GO.classList.add("fill_svg"); 
     SP.classList.add("fill_svg"); 
+    boxFertigran.style.display = "block";
+}
+
+function fertigranBox(){
+    var MG = document.getElementById("MG");
+    var GO = document.getElementById("GO");
+    var SP = document.getElementById("SP");
+    var boxFertigran = document.getElementById("boxFertigran");
+    MG.classList.remove("fill_svg"); 
+    GO.classList.remove("fill_svg"); 
+    SP.classList.remove("fill_svg"); 
+    boxFertigran.style.display = "none";
 }
 
 function fertinor(){
@@ -52,35 +118,89 @@ function fertinor(){
     var MG = document.getElementById("MG");
     var GO = document.getElementById("GO");
     var TO = document.getElementById("TO");
+    var boxFertinor = document.getElementById("boxFertinor");
     BA.classList.add("fill_svg"); 
     SE.classList.add("fill_svg"); 
     AL.classList.add("fill_svg"); 
     MG.classList.add("fill_svg"); 
     GO.classList.add("fill_svg"); 
-    TO.classList.add("fill_svg"); 
+    TO.classList.add("fill_svg");
+    boxFertinor.style.display = "block"; 
+}
+
+function fertinorBox(){
+    var BA = document.getElementById("BA");
+    var SE = document.getElementById("SE");
+    var AL = document.getElementById("AL");
+    var MG = document.getElementById("MG");
+    var GO = document.getElementById("GO");
+    var TO = document.getElementById("TO");
+    var boxFertinor = document.getElementById("boxFertinor");
+    BA.classList.remove("fill_svg"); 
+    SE.classList.remove("fill_svg"); 
+    AL.classList.remove("fill_svg"); 
+    MG.classList.remove("fill_svg"); 
+    GO.classList.remove("fill_svg"); 
+    TO.classList.remove("fill_svg");
+    boxFertinor.style.display = "none"; 
 }
 
 function piratini(){
     var RS = document.getElementById("RS");
     var SC = document.getElementById("SC");
+    var boxPiratini = document.getElementById("boxPiratini");
     RS.classList.add("fill_svg"); 
     SC.classList.add("fill_svg");
+    boxPiratini.style.display = "block"; 
+}
+
+function piratiniBox(){
+    var RS = document.getElementById("RS");
+    var SC = document.getElementById("SC");
+    var boxPiratini = document.getElementById("boxPiratini");
+    RS.classList.remove("fill_svg"); 
+    SC.classList.remove("fill_svg");
+    boxPiratini.style.display = "none"; 
 }
 
 function sudeste(){
     var ES = document.getElementById("ES");
     var RJ = document.getElementById("RJ");
     var MG = document.getElementById("MG");
+    var boxSudeste = document.getElementById("boxSudeste");
     ES.classList.add("fill_svg"); 
     RJ.classList.add("fill_svg");
     MG.classList.add("fill_svg");
+    boxSudeste.style.display = "block";
+}
+
+function sudesteBox(){
+    var ES = document.getElementById("ES");
+    var RJ = document.getElementById("RJ");
+    var MG = document.getElementById("MG");
+    var boxSudeste = document.getElementById("boxSudeste");
+    ES.classList.remove("fill_svg"); 
+    RJ.classList.remove("fill_svg");
+    MG.classList.remove("fill_svg");
+    boxSudeste.style.display = "none";
 }
 
 function bandeirantes(){
     var SP = document.getElementById("SP");
     var MS = document.getElementById("MS");
+    var boxBandeirantes = document.getElementById("boxBandeirantes");
     SP.classList.add("fill_svg"); 
     MS.classList.add("fill_svg");
+    boxBandeirantes.style.display = "block";
+}
+
+function bandeirantesBox(){
+    var SP = document.getElementById("SP");
+    var MS = document.getElementById("MS");
+    var boxBandeirantes = document.getElementById("boxBandeirantes");
+    SP.classList.remove("fill_svg"); 
+    MS.classList.remove("fill_svg");
+    boxBandeirantes.style.display = "none";
 }
 
 function nordeste(){
@@ -88,10 +208,24 @@ function nordeste(){
     var MG = document.getElementById("MG");
     var GO = document.getElementById("GO");
     var TO = document.getElementById("TO");
+    var boxNordeste = document.getElementById("boxNordeste");
     BA.classList.add("fill_svg");
     MG.classList.add("fill_svg");
     GO.classList.add("fill_svg");
     TO.classList.add("fill_svg");
+    boxNordeste.style.display = "block";
+}
+function nordesteBox(){
+    var BA = document.getElementById("BA");
+    var MG = document.getElementById("MG");
+    var GO = document.getElementById("GO");
+    var TO = document.getElementById("TO");
+    var boxNordeste = document.getElementById("boxNordeste");
+    BA.classList.remove("fill_svg");
+    MG.classList.remove("fill_svg");
+    GO.classList.remove("fill_svg");
+    TO.classList.remove("fill_svg");
+    boxNordeste.style.display = "none";
 }
 
 function parana(){
@@ -101,10 +235,29 @@ function parana(){
     var RO = document.getElementById("RO");
     var MS = document.getElementById("MS");
     var PARAGUAI = document.getElementById("PARAGUAI");
+    var boxParana = document.getElementById("boxParana");
     SP.classList.add("fill_svg");
     PR.classList.add("fill_svg");
     SC.classList.add("fill_svg");
     RO.classList.add("fill_svg");
     MS.classList.add("fill_svg");
     PARAGUAI.classList.add("fill_svg");
+    boxParana.style.display = "block";
+}
+
+function paranaBox(){
+    var SP = document.getElementById("SP");
+    var PR = document.getElementById("PR");
+    var SC = document.getElementById("SC");
+    var RO = document.getElementById("RO");
+    var MS = document.getElementById("MS");
+    var PARAGUAI = document.getElementById("PARAGUAI");
+    var boxParana = document.getElementById("boxParana");
+    SP.classList.remove("fill_svg");
+    PR.classList.remove("fill_svg");
+    SC.classList.remove("fill_svg");
+    RO.classList.remove("fill_svg");
+    MS.classList.remove("fill_svg");
+    PARAGUAI.classList.remove("fill_svg");
+    boxParana.style.display = "none";
 }
