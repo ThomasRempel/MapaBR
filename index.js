16fedc28ff78b4f2a4685d6da9d1a99e1a81b37a
David Bonfim <84916984+davidbonfim@users.noreply.github.com>
2022-07-01 16:06:45 -0300

    1.1

---
+++ index.js
@@ -0,0 +1,12 @@
+function fertine() {
+    var PE = document.getElementById("PE");
+    var PB = document.getElementById("PB");
+    var RN = document.getElementById("RN");
+    var CE = document.getElementById("CE");
+    var PI = document.getElementById("PI");
+    PE.classList.add("fill_svg");
+    PB.classList.add("fill_svg");
+    RN.classList.add("fill_svg");
+    CE.classList.add("fill_svg");
+    PI.classList.add("fill_svg");
+}
